bbea6043c786bfe3a6915eccd24157beb0838bbf
mk346 <calebkinyuamunene@gmail.com>
2023-10-27 14:18:19 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./style";
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={` ${styles.paddingX } ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        NavBar
+      </div>
+    </div>
+
+  </div>
+
+);
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,132 @@
+import { l6, boy, cctv, couch, favicon, im4, im_17, im_18, img2, l2, man, net, pamojalogo, photo, tvbox, web3, facebook, instagram, linkedin, twitter, send, shield, star, girl } from "../assets";
+
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "internet",
+        title: "Internet",
+    },
+    {
+        id: "services",
+        title: "Services",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+
+    }
+];
+
+export const features = [
+    {
+        id: "feature-1",
+        icon: star,
+        title: "EXPERTISE",
+        content: "We have expert skills and knowledge in fiber network connections and maintenance",
+    },
+    {
+        id: "feature-2",
+        icon: shield,
+        title: "TRUSTED WORK",
+        content: "Being a certified Internet Service Provider, we ascertain to provide reliable and perfected services",
+    },
+    {
+        id: "feature-3",
+        icon: send,
+        title: "EXPERIENCE SKILLS",
+        content: "We have gained broad experience in our fields of expertise and certain the best service to all our clients",
+    }
+]
+
+export const feedback = [
+    {
+        id: "feedback-1",
+        content: "Since I started using pamoja home fiber I cant complain, their internet is perfect",
+        name: "Daniel Mutemi",
+        title: "Business Manager",
+        img: boy,
+    },
+    {
+        id: "feedback-2",
+        content: "I work from home and pamoja home fiber has been my service provider and its the best",
+        name: "Caroline Njoroge",
+        title: "Entrepreneur",
+        img: girl,
+    },
+    {
+        id: "feedback-2",
+        content: "Pamoja Home Fiber is the best internet service provider out here, its cheap and very reliable",
+        name: "Mohammed Ahmid",
+        title: "Business Man",
+        img: man,
+    },
+
+]
+
+export const stats = [
+    {
+        id: "stats-1",
+        title: "Users Active",
+        value: "3500+",
+    },
+    {
+        id: "stats-2",
+        title: "Trusted by Company",
+        value: "200+",
+    },
+    {
+        id: "stats-3",
+        title: "Daily Connections",
+        value: "20+",
+    },
+]
+
+export const footerLinks = [
+    {
+        title: "Useful Links",
+        links: [
+            {
+                name: "Contact",
+                link: "#",
+            },
+            {
+                name: "Services",
+                link: "#",
+            },
+            {
+                name: "Check Coverage",
+                link: "#",
+            },
+            {
+                name: "Terms & Services",
+                link: "#"
+            }
+        ]
+    }
+]
+
+export const clients = [
+    {
+        id: "social-media-1",
+        icon: instagram,
+        link: "#",
+    },
+    {
+        id: "social-media-2",
+        icon: facebook,
+        link: "#",
+    },
+    {
+        id: "social-media-3",
+        icon: linkedin,
+        link: "#",
+    },
+    {
+        id: "social-media-4",
+        icon: twitter,
+        link: "#",
+    },
+]
